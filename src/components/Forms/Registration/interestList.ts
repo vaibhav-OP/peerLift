dbc6057b2b587d17dbe14892e2257895be06081c
vaibhav-OP <vaibhavyash28@gmail.com>
2023-12-09 13:42:11 +0530

    (Enchanced) community's icons.

--- src/components/Forms/Registration/interestList.ts
+++ src/components/Forms/Registration/interestList.ts
@@ -1,20 +1,31 @@
 import { InterestArray } from "./types";
 
-import Image1 from "@/../public/avatars/1.png";
+import Image1 from "@/../public/communities/1.png";
+import Image2 from "@/../public/communities/2.png";
+import Image3 from "@/../public/communities/3.png";
+import Image4 from "@/../public/communities/4.png";
+import Image5 from "@/../public/communities/5.png";
+import Image6 from "@/../public/communities/6.png";
+import Image7 from "@/../public/communities/7.png";
+import Image8 from "@/../public/communities/8.png";
+import Image9 from "@/../public/communities/9.png";
+import Image10 from "@/../public/communities/10.png";
+import Image11 from "@/../public/communities/11.png";
+import Image12 from "@/../public/communities/12.png";
 
 const InterestList: InterestArray = [
   { name: "Addiction & Relapse", icon: Image1 },
-  { name: "ADHD & ADD", icon: Image1 },
-  { name: "Autism", icon: Image1 },
-  { name: "Depression", icon: Image1 },
-  { name: "Disassociation", icon: Image1 },
-  { name: "Eating Disorder", icon: Image1 },
-  { name: "OCD", icon: Image1 },
-  { name: "Personality Disorder", icon: Image1 },
-  { name: "Physical Problems", icon: Image1 },
-  { name: "Schizophrenia & Psychosis", icon: Image1 },
-  { name: "Stress & Anxiety", icon: Image1 },
-  { name: "Trauma & PTSD", icon: Image1 },
+  { name: "ADHD & ADD", icon: Image2 },
+  { name: "Autism", icon: Image3 },
+  { name: "Depression", icon: Image4 },
+  { name: "Disassociation", icon: Image5 },
+  { name: "Eating Disorder", icon: Image6 },
+  { name: "OCD", icon: Image7 },
+  { name: "Personality Disorder", icon: Image8 },
+  { name: "Physical Problems", icon: Image9 },
+  { name: "Schizophrenia & Psychosis", icon: Image10 },
+  { name: "Stress & Anxiety", icon: Image11 },
+  { name: "Trauma & PTSD", icon: Image12 },
 ];
 
 export default InterestList;
